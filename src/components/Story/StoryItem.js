a21be863be11cab157ec046e041240f5c299da95
박리아 <danny6217@gmail.com>
2024-09-23 22:55:45 +0900

    240923/park/story component init

---
+++ src/components/Story/StoryItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const StoryItem = () => {
+  return (
+    <div>
+      <div>
+        <span>
+          <span>
+            <span></span>
+          </span>
+        </span>
+      </div>
+      <div>
+        <p>CourtneyHenry</p>
+      </div>
+    </div>
+  );
+};
+
+export default StoryItem;
---
+++ src/components/Story/StoryList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 630px;
+  margin: 16px 0 24px;
+  padding: 8px 0;
+  display: flex;
+`;
+
+const StoryList = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default StoryList;
