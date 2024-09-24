0d6783cfe144509b5933a5b758e77923201e7dd7
박리아 <danny6217@gmail.com>
2024-09-25 01:06:50 +0900

    240925/park/finish story

--- src/components/Story/StoryItem.js
+++ src/components/Story/StoryItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import ProfileImg from "../Profile/ProfileImg";
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,47 +10,7 @@ const Wrapper = styled.div`
   gap: 3px;
 `;
 
-const ImgSection = styled.div`
-  border: 1px solid #f00;
-  width: fit-content;
-`;
-
-const ActivationArea = styled.span`
-  display: inline-block;
-  width: 96px;
-  height: 96px;
-  background: linear-gradient(45deg, #f9ce34, #ee2a7b, #6228d7);
-  border-radius: 50%;
-  position: relative;
-`;
-
-const BoundaryLine = styled.span`
-  display: inline-block;
-  width: 90px;
-  height: 90px;
-  background: #fff;
-  border-radius: 50%;
-  position: relative;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const ProfileImg = styled.span`
-  display: inline-block;
-  width: 86px;
-  height: 86px;
-  background: url("/images/userImgs/user123456/profile-photo.png") center/cover
-    no-repeat;
-  border-radius: 50%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const UserNameArea = styled.div`
-  border: 1px solid #f00;
+const UserNameSection = styled.div`
   width: fit-content;
   p {
     font-size: 12px;
@@ -59,16 +20,14 @@ const UserNameArea = styled.div`
 const StoryItem = () => {
   return (
     <Wrapper>
-      <ImgSection>
-        <ActivationArea>
-          <BoundaryLine>
-            <ProfileImg></ProfileImg>
-          </BoundaryLine>
-        </ActivationArea>
-      </ImgSection>
-      <UserNameArea>
+      <ProfileImg
+        url={"/images/userImgs/user123456/profile-photo.jpg"}
+        size={96}
+        type={"active"}
+      />
+      <UserNameSection>
         <p>CourtneyHenry</p>
-      </UserNameArea>
+      </UserNameSection>
     </Wrapper>
   );
 };
--- src/components/Story/StoryList.js
+++ src/components/Story/StoryList.js
@@ -1,18 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 import StoryItem from "./StoryItem";
+import SlideButton from "../common/SlideButton";
 
 const Wrapper = styled.div`
-  width: 630px;
-  margin: 16px 0 24px;
+  width: 1000px;
+  margin: 16px auto 24px;
   padding: 8px 0;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #f00;
 `;
+
+const StoryContent = styled.div`
+  width: 90%;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  border: 1px solid #0f0;
+`;
+
+const styles = {
+  bgsize: 26,
+  bgcolor: "--light-gray-color",
+  fontcolor: "--bg-white-color",
+};
 
 const StoryList = () => {
   return (
     <Wrapper>
-      <StoryItem />
+      <SlideButton type={"left"} {...styles} />
+      <StoryContent>
+        <StoryItem />
+        <StoryItem />
+        <StoryItem />
+      </StoryContent>
+      <SlideButton type={"right"} {...styles} />
     </Wrapper>
   );
 };
