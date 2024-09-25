b756eb8850dea8d61e18d311904b92768cd8fd16
Sheep-heee <sheephi0609@gmail.com>
2024-09-25 17:12:18 +0900

    240925

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import HotHashtag from "./components/HotHashtag";
+import SearchBar from "./components/common/SearchBar";
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <HotHashtag />
     </div>
   );
