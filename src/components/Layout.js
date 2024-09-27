348f2c49f708ae388f31fbcd2bafbc6a9b0d9a0b
Sheep-heee <sheephi0609@gmail.com>
2024-09-27 18:20:01 +0900

    240927

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
-      <Outlet/>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
