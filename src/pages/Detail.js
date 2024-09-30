f1c00410a737c5b7dc4156e8c1401b3b34cf062a
park-ria <danny6217@gmail.com>
2024-09-30 17:50:26 +0900

    240930

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import ClickFeed from "../components/Detail/ClickFeed"
-import Follower from "../components/Detail/Follower"
+import ClickFeed from "../components/Detail/ClickFeed";
+import Follower from "../components/Detail/Follower";
 
 const Wrapper = styled.div``;
 
 const Detail = () => {
   return (
     <Wrapper>
-      <ClickFeed />
-      <Follower/>
+      {/* <ClickFeed /> */}
+      <Follower />
     </Wrapper>
   );
 };
