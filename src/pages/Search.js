5eda883d738e66c075b4ebf5734b135be56b3b98
Sheep-heee <sheephi0609@gmail.com>
2024-10-08 16:27:03 +0900

    241008

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import MyPostItem from "../components/MyFeed/MyPostItem";
+import { LuMoreHorizontal } from "react-icons/lu";
+import MainHeader from "../components/Main/MainHeader";
 
 const Wrapper = styled.div`
   width: 100%;
+  background: ${({ theme }) => theme.bgColor};
+`;
+
+const Margin = styled.div`
+  height: 185px;
 `;
 
 const Container = styled.div`
@@ -11,9 +19,16 @@ const Container = styled.div`
 `;
 
 const Header = styled.div`
-  width: 100%;
+  width: 936px;
   height: 100px;
-  border: 1px solid #f00;
+  display: flex;
+  padding: 0 40px;
+  justify-content: space-between;
+  align-items: center;
+  background: ${({ theme }) => theme.bgColor};
+  position: fixed;
+  top: 85px;
+  color: ${({ theme }) => theme.fontColor};
 `;
 
 const Keyword = styled.div`
@@ -21,13 +36,35 @@ const Keyword = styled.div`
   font-weight: var(--font-bold);
 `;
 
+const ItemArea = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 8px;
+`;
+
+const mockUp = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24,
+];
+
 const Search = () => {
   return (
     <Wrapper>
+      <MainHeader />
+      <Margin />
       <Container>
         <Header>
           <Keyword>#여행</Keyword>
+          <LuMoreHorizontal size={22} />
         </Header>
+        <ItemArea>
+          {mockUp.map((it) => (
+            <MyPostItem
+              url={`/images/userImgs/bIqUJE0DxQa1HdkLfMqh105VOrQ/feed${it}.jpg`}
+            />
+          ))}
+        </ItemArea>
       </Container>
     </Wrapper>
   );
